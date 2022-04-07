e023597cb6f02b18418e95de2e951b02fa93a1c4
operdigao <rauper@gmail.com>
2022-04-07 10:52:04 -0300

    Create __init__.py

---
+++ Fórmula Mágica de Joel Greenblatt/interface/__init__.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+
+def linha(tam=80):
+    return '-' * tam
+
+
+def cabeçalho(txt):
+    sleep(1.5)
+    print(linha())
+    print(txt.center(80))
+    print(linha())
+    sleep(2)
